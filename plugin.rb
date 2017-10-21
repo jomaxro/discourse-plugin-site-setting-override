c6d912f6c6d134647fcf6448575d552f8053320a
Joshua Rosenfeld <jomaxro@discourse.org>
2017-10-21 11:05:46 -0400

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: discourse-plugin-site-setting-override
+# about: Example plugin to override a site setting
+# version: 1.0.0
+# authors: Joshua Rosenfeld
+# url: https://github.com/jomaxro/discourse-plugin-site-setting-override
